fd32907e6e29671e2e0f3fd3fc280e5f1d899bb7
lcalcagni <l.calcagni@gmail.com>
2018-05-14 16:43:49 -0300

    Update read_data.py

--- functions/read_data.py
+++ functions/read_data.py
@@ -7,11 +7,11 @@ def read_from_shower(angle, type, number, data):
     if  (data == 'Primary particle' and data in line):
         prim_type = line.split(':')[-1].strip()
         prim_type = prim_type.split('^')[0]
-	return prim_type
+    return prim_type
         
     if  (data == 'Primary energy' and data in line) :
         prim_egy = line.split(':')[-1].strip()
         prim_egy = prim_egy.replace(' ','')
-	return prim_egy
+    return prim_egy
 
    
