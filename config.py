f5664daa8bdcb5d8bd9b0b4514a97a02d4c47dde
Laura Calcagni <l.calcagni@gmail.com>
2020-10-03 20:35:38 -0300

    Handle 503 code

--- config.py
+++ config.py
@@ -2,5 +2,5 @@ OAI             = "{http://www.openarchives.org/OAI/2.0/}"
 ARXIV           = "{http://arxiv.org/OAI/arXiv/}"
 
 
-arxiv_date      = ['2012-01-01', '2012-02-01']  #from, to
-arxiv_category  = 'physics:hep-ph'
+arxiv_date      = ['2012-01-01', '2012-01-03']  #from, to
+arxiv_category  = "physics:hep-ph"
--- endpoints.py
+++ endpoints.py
@@ -1,14 +1,26 @@
 from config import *
 import requests
+import json
+import re
+import time
+from log import logger
 
 
 def get_papers(category, date):
     base_url = 'http://export.arxiv.org/oai2?verb=ListRecords&'
     url = (base_url +
            f"from={date[0]}&until={date[1]}&" +
-           "metadataPrefix=arXiv&set={category}")
+           f"metadataPrefix=arXiv&set={category}")
     while True:
-        papers = requests.get(url)
-        papers = papers.text.encode('utf8')
-        print(papers)
+        try:
+            response = requests.get(url)
+            print("fetching...", url)
+            if response.status_code == 503:
+                pattern = "Retry after (.*?) seconds"
+                seconds_wait = int(re.search(pattern, response.text).group(1))
+                print(f"Got 503. Retrying after {seconds_wait} seconds")
+                time.sleep(seconds_wait)
+                continue
+        except:
+            raise
     return papers
