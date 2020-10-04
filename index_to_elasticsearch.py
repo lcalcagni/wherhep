9ee8fa448b50dcba182d8c74f804b7cded3666e7
Laura Calcagni <l.calcagni@gmail.com>
2020-10-04 17:52:18 -0300

    Index data to elasticsarch instance

---
+++ index_to_elasticsearch.py
@@ -0,0 +1,13 @@
+import requests
+import os
+import sys, json
+from elasticsearch import Elasticsearch, helpers
+
+
+es = Elasticsearch([os.environ['ELASTICSEARCH_INSTANCE']])
+
+filename = '.output.json'
+data = [json.loads(line) for line in open(filename, 'r')]
+helpers.bulk(es, data, index='title', doc_type='HEP_papers')
+
+indices=es.indices.get_alias().keys()
